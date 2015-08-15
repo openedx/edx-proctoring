01abf5e87dc90ac738bcb900a54f78c282cb1623
Chris Dodge <cdodge@edx.org>
2015-08-15 15:43:23 -0400

    make links open up in new tab

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.6.1',
+    version='0.6.2',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
