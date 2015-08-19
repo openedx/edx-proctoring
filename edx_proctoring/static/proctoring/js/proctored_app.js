1b4341c9ed7dd9ec0cecee36bba417c457461eb5
Muhammad Shoaib <muhammad.shoaib@arbisoft.com>
2015-08-19 20:12:08 +0500

    moved the scroll event in the proctored_exam_view. js file

--- edx_proctoring/static/proctoring/js/proctored_app.js
+++ edx_proctoring/static/proctoring/js/proctored_app.js
@@ -5,16 +5,4 @@ $(function() {
         model: new ProctoredExamModel()
     });
     proctored_exam_view.render();
-
-    // show the timer bar when the window scrolls.
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 60){
-            $(".proctored_exam_status").addClass('fixed');
-            $(".wrapper-course-material").css('margin-top', '58px');
-        }
-        else {
-            $(".proctored_exam_status").removeClass('fixed');
-            $(".wrapper-course-material").css('margin-top', '0');
-        }
-    });
 });
