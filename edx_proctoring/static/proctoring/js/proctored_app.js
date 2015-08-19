49cc3eff902c4e7af7d8b0ebdcdc02c25db03ccc
Muhammad Shoaib <muhammad.shoaib@arbisoft.com>
2015-08-19 15:35:16 +0500

    added the javascript for the window scroll and timer bar will be visible when scrolling the window

--- edx_proctoring/static/proctoring/js/proctored_app.js
+++ edx_proctoring/static/proctoring/js/proctored_app.js
@@ -5,4 +5,16 @@ $(function() {
         model: new ProctoredExamModel()
     });
     proctored_exam_view.render();
+
+    // show the timer bar when the window scrolls.
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 60){
+            $(".proctored_exam_status").addClass('fixed');
+            $(".wrapper-course-material").css('margin-top', '58px');
+        }
+        else {
+            $(".proctored_exam_status").removeClass('fixed');
+            $(".wrapper-course-material").css('margin-top', '0');
+        }
+    });
 });
