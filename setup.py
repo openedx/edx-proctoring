cd2ad3b339fb94bd9eaefb5135610f1fe227efc7
Will Daly <will@edx.org>
2015-08-31 07:44:07 -0700

    Upgrade django-rest-framework to v3.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.9.6b',
+    version='0.9.9',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
