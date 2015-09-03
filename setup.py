34f3269160851bceca6c23c076243defb3b3ecb1
Chris Dodge <cdodge@edx.org>
2015-09-02 22:06:40 -0400

    update version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.8.0',
+    version='0.8.1',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
