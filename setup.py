8ec0f3f88ecfa6cb64ff11f5be5a1b12903d082c
Chris Dodge <cdodge@edx.org>
2015-09-30 10:05:23 -0400

    Practice exams now should not have any icon associated with it

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.9.13',
+    version='0.9.14',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
