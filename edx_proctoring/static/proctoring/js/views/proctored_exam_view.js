666db8ffbb851f803660cda9c6a9e3eb57bca41d
Muhammad Shoaib <muhammad.shoaib@arbisoft.com>
2015-10-28 17:32:57 +0500

    PHX-159 - Hides the courseware navigation/pane for the end user when the exam timer bar is visible/running

--- edx_proctoring/static/proctoring/js/views/proctored_exam_view.js
+++ edx_proctoring/static/proctoring/js/views/proctored_exam_view.js
@@ -77,6 +77,10 @@ var edx = edx || {};
 
             this.render();
         },
+        hideCourseNavBar: function() {
+            $(".wrapper-course-material").addClass('invisible');
+            $('.course-wrapper > .course-index').addClass('hidden');
+        },
         render: function () {
             if (this.template !== null) {
                 if (
@@ -85,6 +89,7 @@ var edx = edx || {};
                     this.model.get('attempt_status') !== 'error'
                 ) {
                     // add callback on scroll event
+                    this.hideCourseNavBar();
                     $(window).bind('scroll', this.detectScroll);
 
                     var html = this.template(this.model.toJSON());
