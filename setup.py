b757a025fce43b8bc67ac0c7fafe0e0c7b5e7714
Chris Dodge <cdodge@edx.org>
2015-10-29 22:17:21 -0400

    remove unneeded anchor tags

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.10.14',
+    version='0.10.15',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
