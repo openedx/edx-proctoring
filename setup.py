a6180f599d4676776ccf6c03d6d3deafaeab7114
Ned Batchelder <ned@edx.org>
2015-11-10 14:33:02 -0500

    0.11.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.11.1',
+    version='0.11.2',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
