2aad481146a3f8ca49a786f9230caea923faf412
Chris Dodge <cdodge@edx.org>
2015-12-16 13:56:17 -0500

    bump version to 0.12.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.12.1',
+    version='0.12.2',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
