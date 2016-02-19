8bff53ee391438ed2bd6fe5ab076c8209efee30e
Douglas Hall <dhall@edx.org>
2016-02-18 21:02:02 -0500

    Incrementing version for 0.12.10 release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-proctoring',
-    version='0.12.9',
+    version='0.12.10',
     description='Proctoring subsystem for Open edX',
     long_description=open('README.md').read(),
     author='edX',
