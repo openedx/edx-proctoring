64181b58406947fa217b680c5c871eeb6aa9282b
Tyler Hallada <thallada@edx.org>
2017-08-02 15:28:21 -0400

    Bump version to 0.18.3

--- edx_proctoring/__init__.py
+++ edx_proctoring/__init__.py
@@ -4,6 +4,6 @@ The exam proctoring subsystem for the Open edX platform.
 
 from __future__ import absolute_import
 
-__version__ = '0.18.2'
+__version__ = '0.18.3'
 
 default_app_config = 'edx_proctoring.apps.EdxProctoringConfig'  # pylint: disable=invalid-name
