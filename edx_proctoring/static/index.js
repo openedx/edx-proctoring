79d83f36e467b5f5d8ae9337679e158b3d362fbf
Matt Hughes <mhughes@edx.org>
2018-12-05 15:29:39 -0500

    Hook up failure conditions for ping/echo

--- edx_proctoring/static/index.js
+++ edx_proctoring/static/index.js
@@ -9,19 +9,25 @@ export const handlerWrapper = (Handler) => {
       }
       case 'startExamAttempt': {
         if(handler.onStartExamAttempt) {
-          handler.onStartExamAttempt().then(() => self.postMessage({type: 'examAttemptStarted'}))
+          handler.onStartExamAttempt()
+                 .then(() => self.postMessage({type: 'examAttemptStarted'}))
+                 .catch(() => self.postMessage({type: 'examAttemptStartFailed'}))
         }
         break;
       }
       case 'endExamAttempt': {
         if(handler.onEndExamAttempt) {
-          handler.onEndExamAttempt().then(() => self.postMessage({type: 'examAttemptEnded'}))
+          handler.onEndExamAttempt()
+                 .then(() => self.postMessage({type: 'examAttemptEnded'}))
+                 .catch(() => self.postMessage({type: 'examAttemptEndFailed'}))
         }
         break;
       }
       case 'ping': {
         if(handler.onPing) {
-          handler.onPing().then(() => self.postMessage({type: 'echo'}))
+          handler.onPing()
+                 .then(() => self.postMessage({type: 'echo'}))
+                 .catch(() => self.postMessage({type: 'pingFailed'}))
         }
         break;
       }
