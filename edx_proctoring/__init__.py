369a99b049081344b934d93718db41c491e8d9fa
Matt Hughes <mhughes@edx.org>
2018-12-06 14:48:42 -0500

    Consolidate version numbers to be the same for npm & PyPI

--- edx_proctoring/__init__.py
+++ edx_proctoring/__init__.py
@@ -4,6 +4,7 @@ The exam proctoring subsystem for the Open edX platform.
 
 from __future__ import absolute_import
 
-__version__ = '1.5.0b3'
+# Be sure to update the version in package.json when incrementing this! The two should stay in sync.
+__version__ = '1.5.0b4'
 
 default_app_config = 'edx_proctoring.apps.EdxProctoringConfig'  # pylint: disable=invalid-name
