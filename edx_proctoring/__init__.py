3f12598c597d7b7f4f0a7dcd16914ddc7ad6767f
Dave St.Germain <dstgermain@edx.org>
2018-12-06 14:54:11 -0500

    Synchronize version numbers between JS and Python packages

--- edx_proctoring/__init__.py
+++ edx_proctoring/__init__.py
@@ -4,6 +4,7 @@ The exam proctoring subsystem for the Open edX platform.
 
 from __future__ import absolute_import
 
-__version__ = '1.5.0'
+# Be sure to update the version number in edx_proctoring/package.json
+__version__ = '1.5.0rc1'
 
 default_app_config = 'edx_proctoring.apps.EdxProctoringConfig'  # pylint: disable=invalid-name
