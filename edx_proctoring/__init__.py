80e3ede370f8202f23ccdf04a07917dc345fd8a7
Michael Roytman <mroytman@edx.org>
2021-04-13 13:07:59 -0400

    fix: Fix JWT Encoding Bug with Version 2.0.1 of pyjwt[crypto] Library
    Version 2.0.1 of the pyjwt library includes a change to the jwt.encode(...) return type. The method now returns tokens as strings instead of byte strings. In the backend base class BaseRestProctoringProvider, code was originally decoding the token as utf-8. When using version 2.0.1 of the pyjwt[crypto] library, this code was raising a "AttributeError: 'str' object has no attribute 'decode'" error. This code fixes this behavior to treat the return type of jwt.encode(...) as a string.

--- edx_proctoring/__init__.py
+++ edx_proctoring/__init__.py
@@ -3,6 +3,6 @@ The exam proctoring subsystem for the Open edX platform.
 """
 
 # Be sure to update the version number in edx_proctoring/package.json
-__version__ = '3.8.5'
+__version__ = '3.8.6'
 
 default_app_config = 'edx_proctoring.apps.EdxProctoringConfig'  # pylint: disable=invalid-name
