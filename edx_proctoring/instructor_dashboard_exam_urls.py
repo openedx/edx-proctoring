7ff8af0936d83a0eeba4f5a55f1ef14a12733ada
Jawayria <jawayriahashmi@gmail.com>
2022-04-04 20:59:16 +0500

    fix: replace url with re_path

--- edx_proctoring/instructor_dashboard_exam_urls.py
+++ edx_proctoring/instructor_dashboard_exam_urls.py
@@ -3,7 +3,7 @@ URL mapping for the exam instructor dashboard.
 """
 
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path
 
 from edx_proctoring import views
 
@@ -11,7 +11,7 @@ app_name = 'instructor'
 
 
 urlpatterns = [
-    url(
+    re_path(
         fr'edx_proctoring/v1/instructor/{settings.COURSE_ID_PATTERN}/(?P<exam_id>\d+)$',
         views.InstructorDashboard.as_view(),
         name='instructor_dashboard_exam'
