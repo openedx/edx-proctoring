79fff3fbbf6b11191beba51f94a4b307d6fdb051
Alie Langston <alangsto@wellesley.edu>
2023-04-19 09:39:58 -0400

    fix: user id shoulbe an integer, not a string

--- edx_proctoring/__init__.py
+++ edx_proctoring/__init__.py
@@ -3,6 +3,6 @@ The exam proctoring subsystem for the Open edX platform.
 """
 
 # Be sure to update the version number in edx_proctoring/package.json
-__version__ = '4.15.0'
+__version__ = '4.15.1'
 
 default_app_config = 'edx_proctoring.apps.EdxProctoringConfig'  # pylint: disable=invalid-name
