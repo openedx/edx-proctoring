63d161163f2c334552c119da4205d7462174c237
Irtaza Akram <irtaza.akram@arbisoft.com>
2024-04-15 11:00:20 +0500

    feat: add support for python 3.11 & 3.12

--- setup.py
+++ setup.py
@@ -71,13 +71,13 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
-        'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     packages=[
         'edx_proctoring',
